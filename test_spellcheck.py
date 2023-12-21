a5385ab07c5d2bf07d7f4cb4dbf482fb261c8d55
talk2tuneeh <78221450+talk2tuneeh@users.noreply.github.com>
2023-12-21 17:20:22 +0100

    Add files via upload

---
+++ test_spellcheck.py
@@ -0,0 +1,32 @@
+
+import spellcheck
+import pytest
+
+# String variables to be tested
+alpha = "Checking the length & structure of the sentence."
+#beta = "This sentence should fail the test"
+
+# Do not delete this function. You may change the value assigned to input to test different inputs to your test functions.
+@pytest.fixture
+def input_value():
+    input = alpha
+    return input
+
+# First test function test_length()
+def test_length(input_value):
+    
+    assert (spellcheck.word_count(input_value) < 10)
+    assert (spellcheck.char_count(input_value) < 50)
+
+    #raise NotImplementedError()
+
+# Second test function test_struc()
+def test_struc(input_value):
+    
+    ### WRITE SOLUTION CODE HERE
+    assert (spellcheck.first_char(input_value).isupper())
+    assert (spellcheck.last_char(input_value) == '.')
+
+    #raise NotImplementedError()
+
+# Run these tests with `python3 -m pytest test_spellcheck.py`
